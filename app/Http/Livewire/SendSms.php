654052721f431877d269cb409858681dd8bb4553
johnreynaceda <johnreynaceda@sksu.edu.ph>
2023-07-30 12:10:05 +0800

    send to all barangay

--- app/Http/Livewire/SendSms.php
+++ app/Http/Livewire/SendSms.php
@@ -38,42 +38,42 @@ class SendSms extends Component
 
         $residents = Resident::where('barangay', $this->barangay)->pluck('contact_number')->toArray();
 
-        if ($this->is_checked == null) {
-            $numbers = implode(',', $residents);
-        } else {
-            $numbers = implode(',', collect($this->is_checked)->pluck('number')->toArray());
-        }
-        $api_key = '1aaad08e0678a1c60ce55ad2000be5bd';
-        $sender = 'SEMAPHORE';
-        $ch = curl_init();
-        $parameters = [
-            'apikey' => $api_key,
-            'number' => $numbers,
-            'message' => $this->message,
-            'sendername' => $sender,
-        ];
-        curl_setopt(
-            $ch,
-            CURLOPT_URL,
-            'https://semaphore.co/api/v4/messages'
-        );
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt(
-            $ch,
-            CURLOPT_POSTFIELDS,
-            http_build_query($parameters)
-        );
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $output = curl_exec($ch);
-        curl_close($ch);
+        // if ($this->is_checked == null) {
+        //     $numbers = implode(',', $residents);
+        // } else {
+        //     $numbers = implode(',', collect($this->is_checked)->pluck('number')->toArray());
+        // }
+        // $api_key = '1aaad08e0678a1c60ce55ad2000be5bd';
+        // $sender = 'SEMAPHORE';
+        // $ch = curl_init();
+        // $parameters = [
+        //     'apikey' => $api_key,
+        //     'number' => $numbers,
+        //     'message' => $this->message,
+        //     'sendername' => $sender,
+        // ];
+        // curl_setopt(
+        //     $ch,
+        //     CURLOPT_URL,
+        //     'https://semaphore.co/api/v4/messages'
+        // );
+        // curl_setopt($ch, CURLOPT_POST, 1);
+        // curl_setopt(
+        //     $ch,
+        //     CURLOPT_POSTFIELDS,
+        //     http_build_query($parameters)
+        // );
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // $output = curl_exec($ch);
+        // curl_close($ch);
 
-        $this->dialog()->success(
-            $title = 'Message Sent',
-            $description = 'Message has been sent to the residents of ' . $this->barangay . '. Thank You!'
-        );
-        $this->reset('barangay', 'message');
+        // $this->dialog()->success(
+        //     $title = 'Message Sent',
+        //     $description = 'Message has been sent to the residents of ' . $this->barangay . '. Thank You!'
+        // );
+        // $this->reset('barangay', 'message');
 
-        return $output;
+        // return $output;
 
 
     }
@@ -151,5 +151,14 @@ class SendSms extends Component
             ];
         }
 
+    }
+
+    public function sendToAll()
+    {
+        $this->validate([
+            'message' => 'required',
+        ]);
+
+        $residents = Resident::pluck('contact_number')->toArray();
     }
 }
--- resources/views/livewire/send-sms.blade.php
+++ resources/views/livewire/send-sms.blade.php
@@ -3,45 +3,49 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white bg-opacity-80 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <div class="w-96">
-                        <x-native-select label="Barangay List" wire:model="barangay">
-                            <option selected hidden>Select an Option</option>
-                            <option value="ANILAO EAST">ANILAO EAST</option>
-                            <option value="ANILAO PROPER">ANILAO PROPER</option>
-                            <option value="BAGALANGIT">BAGALANGIT</option>
-                            <option value="BULACAN">BULACAN</option>
-                            <option value="CALAMIAS">CALAMIAS</option>
-                            <option value="ESTRELLA">ESTRELLA</option>
-                            <option value="GASANG">GASANG</option>
-                            <option value="LAUREL">LAUREL</option>
-                            <option value="LIGAYA">LIGAYA</option>
-                            <option value="MAINAGA">MAINAGA</option>
-                            <option value="MAINIT">MAINIT</option>
-                            <option value="MAJUBEN">MAJUBEN</option>
-                            <option value="MALIMATOC I">MALIMATOC I</option>
-                            <option value="MALIMATOC II">MALIMATOC II</option>
-                            <option value="NAG-IBA">NAG-IBA</option>
-                            <option value="PILAHAN">PILAHAN</option>
-                            <option value="POBLACION">POBLACION</option>
-                            <option value="PULANG LUPA">PULANG LUPA</option>
-                            <option value="PULONG ANAHAO">PULONG ANAHAO</option>
-                            <option value="PULONG BALIBAGUHAN">PULONG BALIBAGUHAN</option>
-                            <option value="PULONG NIOGAN">PULONG NIOGAN</option>
-                            <option value="SAGUING">SAGUING</option>
-                            <option value="SAMPAGUITA">SAMPAGUITA</option>
-                            <option value="SAN FRANCISCO">SAN FRANCISCO</option>
-                            <option value="SAN JOSE">SAN JOSE</option>
-                            <option value="SAN JUAN">SAN JUAN</option>
-                            <option value="SAN TEODORO">SAN TEODORO</option>
-                            <option value="SANTA ANA">SANTA ANA</option>
-                            <option value="SANTA MESA">SANTA MESA</option>
-                            <option value="SANTO NINO">SANTO NINO</option>
-                            <option value="SANTO TOMAS">SANTO TOMAS</option>
-                            <option value="SOLO">SOLO</option>
-                            <option value="TALAGA EAST">TALAGA EAST</option>
-                            <option value="TALAGA PROPER">TALAGA PROPER</option>
+                    <div class="flex space-x-3 items-end">
+                        <div class="w-96">
+                            <x-native-select label="Barangay List" wire:model="barangay">
+                                <option selected hidden>Select an Option</option>
+                                <option value="ANILAO EAST">ANILAO EAST</option>
+                                <option value="ANILAO PROPER">ANILAO PROPER</option>
+                                <option value="BAGALANGIT">BAGALANGIT</option>
+                                <option value="BULACAN">BULACAN</option>
+                                <option value="CALAMIAS">CALAMIAS</option>
+                                <option value="ESTRELLA">ESTRELLA</option>
+                                <option value="GASANG">GASANG</option>
+                                <option value="LAUREL">LAUREL</option>
+                                <option value="LIGAYA">LIGAYA</option>
+                                <option value="MAINAGA">MAINAGA</option>
+                                <option value="MAINIT">MAINIT</option>
+                                <option value="MAJUBEN">MAJUBEN</option>
+                                <option value="MALIMATOC I">MALIMATOC I</option>
+                                <option value="MALIMATOC II">MALIMATOC II</option>
+                                <option value="NAG-IBA">NAG-IBA</option>
+                                <option value="PILAHAN">PILAHAN</option>
+                                <option value="POBLACION">POBLACION</option>
+                                <option value="PULANG LUPA">PULANG LUPA</option>
+                                <option value="PULONG ANAHAO">PULONG ANAHAO</option>
+                                <option value="PULONG BALIBAGUHAN">PULONG BALIBAGUHAN</option>
+                                <option value="PULONG NIOGAN">PULONG NIOGAN</option>
+                                <option value="SAGUING">SAGUING</option>
+                                <option value="SAMPAGUITA">SAMPAGUITA</option>
+                                <option value="SAN FRANCISCO">SAN FRANCISCO</option>
+                                <option value="SAN JOSE">SAN JOSE</option>
+                                <option value="SAN JUAN">SAN JUAN</option>
+                                <option value="SAN TEODORO">SAN TEODORO</option>
+                                <option value="SANTA ANA">SANTA ANA</option>
+                                <option value="SANTA MESA">SANTA MESA</option>
+                                <option value="SANTO NINO">SANTO NINO</option>
+                                <option value="SANTO TOMAS">SANTO TOMAS</option>
+                                <option value="SOLO">SOLO</option>
+                                <option value="TALAGA EAST">TALAGA EAST</option>
+                                <option value="TALAGA PROPER">TALAGA PROPER</option>
 
-                        </x-native-select>
+                            </x-native-select>
+                        </div>
+                        <x-button label="Send to all barangay" right-icon="annotation" positive rounded
+                            wier:click="{{}}" />
                     </div>
                     <div class="mt-3">
                         <div class="flex justify-between items-center">
